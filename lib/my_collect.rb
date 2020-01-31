80a4f881ff978276b0351a44713005dbda198389
Curtis pachtinger <cpach96@gmail.com>
2020-01-31 14:47:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+require 'pry'
+def my_collect(collection)
+  i = 0 
+  new_array = []
+    while i < collection.length 
+      yield(collection[i])
+      i = i + 1 
+  end
+  new_array
+end
 
-
+my_collect(collection) do |up|
+  up.upcase
+end
+ 
