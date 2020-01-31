7ff1cac5a3da837d81091ffda1634349b4d0b322
Curtis pachtinger <cpach96@gmail.com>
2020-01-31 15:14:24 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,13 +3,12 @@ def my_collect(collection)
   new_array = []
   i = 0 
     while i < collection.length 
-      yield(collection[i])
+     new_array.push yield(collection[i])
       i = i + 1 
   end
   collection
 end
 
 
-my_collect(new_array) {|n| n.upcase}
 
  
